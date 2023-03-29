808e045b7068c0d92ef000df9bd27217947c69d4
angie <75858331+ahalsmer@users.noreply.github.com>
2023-03-29 19:34:52 -0400

    Add project files.

---
+++ ToDoApp/Models/ToDo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Models
+{
+    internal class ToDo
+    {
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ ToDoApp/ToDoPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDoPage : ContentPage
+    {
+        public ToDoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveButton_Clicked(object sender, EventArgs e)
+        {
+            var todo = new ToDo();
+            todo.Text = ToDoText.Text;
+        }
+
+        private void OnDeleteButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
